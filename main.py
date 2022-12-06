372c55fcbc88d936c76282eca1c6c51f094f4548
k2supra <andrijkorolevic@gmail.com>
2022-12-06 19:50:14 +0200

    Created new task 3

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello dev")
+class Music_instrument:
+    name = ''
+    category = ''
+    price = 0
+    size = ''
+
+    def __init__(self):
+        print(f"Created objects of MI:")
+        self.name = "Triangle"
+        self.category = 'hit'
+        self.price = 9999999
+        self.size = '236m'
+
+
+    def ShowOn(self):
+        print(f"Name: {self.name} \nCategory: {self.category} \nPrice: {self.price}$ \nSize: {self.size}")
+    def __del__(self):
+        print("Deleted objects of MI")
+
+if __name__ == '__main__':
+    music_instrument = Music_instrument()
+    music_instrument.ShowOn()
+    del music_instrument
